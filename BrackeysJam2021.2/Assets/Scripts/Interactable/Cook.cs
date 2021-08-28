10aa6a659f55ad32537bb5ce1106436de1129114
Marc <58913562+MarcMasVidal@users.noreply.github.com>
2021-08-28 10:22:38 +0200

    Audio potion

--- BrackeysJam2021.2/Assets/Scripts/Interactable/Cook.cs
+++ BrackeysJam2021.2/Assets/Scripts/Interactable/Cook.cs
@@ -8,12 +8,46 @@ public class Cook : Interactable
     private Transform spawnerPoint;
     [SerializeField]
     private OrdersManager ordersManager;
+
+    private float currentCooldown = 0;
+    public float cooldown;
+    private bool activate = false;
+    GameObject potion;
+
+    public delegate void StartCook();
+    public static StartCook CookStart;
+
     public override void Active()
     {
-        var potion = ordersManager.PotionDone();
+        currentCooldown = cooldown;
+        activate = true;
+        potion = ordersManager.PotionDone();
         if (!(potion is null))
         {
-            Instantiate(potion, spawnerPoint.position, Quaternion.identity);
+            CookStart?.Invoke();
         }
+    }
+    private void Update()
+    {
+        if (activate)
+        {
+            if (!(potion is null))
+            {
+                currentCooldown -= Time.deltaTime;
+                if (currentCooldown < 0)
+                {
+                    currentCooldown = cooldown;
+                    activate = false;
+
+                    Instantiate(potion, spawnerPoint.position, Quaternion.identity);
+                }
+            }
+            else
+            {
+                currentCooldown = cooldown;
+                activate = false;
+            }
+        }
+
     }
 }
